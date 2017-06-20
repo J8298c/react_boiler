d1c3770ba5148a2c1a51f0418b6b0fe995b35f56
j8298c <juliocmojica@gmail.com>
2017-06-19 21:22:41 -0400

    initial commit with htmlwebpackplugin react react dom and webpack plus loaders

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPluginConfig = new HtmlWebPackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
+        ]
+    },
+    plugins: [HtmlWebPackPluginConfig]
+}
